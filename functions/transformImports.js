9549d94b934377b255cfafb637ac96c8510eabe1
mac <ozgr@live.com>
2024-06-24 14:34:43 +0300

    Add transform imports function to convert import statements

---
+++ functions/transformImports.js
@@ -0,0 +1,36 @@
+export default code => {
+  // Regular expression to match import statements from 'react'
+  const importRegex =
+    /import\s+(?:(\w+)\s*,?\s*)?(?:{([^}]+)})?\s+from\s+['"]react['"];?/g
+
+  let transformedCode = code
+  let match
+  let defaultImport = ''
+  const namedImports = new Set()
+
+  // Collect all imports
+  while ((match = importRegex.exec(code)) !== null) {
+    if (match[1]) {
+      defaultImport = match[1].trim()
+    }
+    if (match[2]) {
+      match[2].split(',').forEach((imp) => namedImports.add(imp.trim()))
+    }
+  }
+
+  // Remove original import statements
+  transformedCode = transformedCode.replace(importRegex, '')
+
+  // Add transformed imports at the beginning of the code
+  let newImports = ''
+  if (defaultImport) {
+    newImports += `const ${defaultImport} = window.React;\n`
+  } else {
+    newImports += 'const React = window.React;\n'
+  }
+  if (namedImports.size > 0) {
+    newImports += `const { ${Array.from(namedImports).join(', ')} } = React;\n`
+  }
+
+  return newImports + transformedCode
+}
