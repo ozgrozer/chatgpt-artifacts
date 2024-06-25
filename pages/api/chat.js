c6f5bfe4306493485adbe7ff372a76fec4f81790
mac <ozgr@live.com>
2024-06-25 04:01:39 +0300

    Add system message

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -11,7 +11,10 @@ export default async (req, res) => {
     const stream = await openai.chat.completions.create({
       stream: true,
       model: 'gpt-4o',
-      messages: [{ role: 'user', content: prompt }]
+      messages: [
+        { role: 'system', content: 'If user is asking for a React.js related question then don\'t talk about how to install React and stuff. Just give the jsx and css as different code blocks. But always combine all the jsx in one code block and all the css in one code block. Pay attention to the details in css.' },
+        { role: 'user', content: prompt }
+      ]
     })
 
     return new Response(
