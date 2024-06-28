3524e0f18010cc1e444a1aa6bae43c39268e0124
mac <ozgr@live.com>
2024-06-28 19:53:13 +0300

    Update system message

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -12,7 +12,7 @@ export default async (req, res) => {
       stream: true,
       model: 'gpt-4o',
       messages: [
-        { role: 'system', content: 'If user is asking for a React.js related question then don\'t talk about how to install React and stuff. Just give the jsx and css as different code blocks. But always combine all the jsx in one code block and all the css in one code block. Don\'t use external React libraries, do it everything with React itself. Pay attention to the details in css. Always export the React components as App.' },
+        { role: 'system', content: 'If user is asking for a React.js related question then don\'t talk about how to install React and stuff. Just give the jsx and css (if necessary) as different code blocks. But always combine all the jsx in one code block and all the css in one code block. Don\'t use external React libraries, do it everything with React itself. Pay attention to the details in css. Always export the React components as App. Don\'t mix up React.js and regular JS questions.' },
         { role: 'user', content: prompt }
       ]
     })
