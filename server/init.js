4657e2ee890fc270a4a9dfef06f12a47880fa1c2
mac <ozgr@live.com>
2024-07-03 22:22:07 +0300

    Show current time

--- server/init.js
+++ server/init.js
@@ -9,6 +9,14 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const showCurrentTime = () => {
+  const now = new Date()
+  const hours = String(now.getHours()).padStart(2, '0')
+  const minutes = String(now.getMinutes()).padStart(2, '0')
+  const seconds = String(now.getSeconds()).padStart(2, '0')
+  return `${hours}:${minutes}:${seconds}`
+}
+
 app.prepare().then(() => {
   const server = createServer((req, res) => {
     if (req.url === '/') {
@@ -22,7 +30,13 @@ app.prepare().then(() => {
 
   io.on('connection', socket => {
     socket.on('codeBlocks', codeBlocks => {
-      execute({ socket, codeBlocks })
+      const sendMessage = message => {
+        const _message = `${showCurrentTime()}: ${message}`
+        console.log(_message)
+        io.emit('codeBlocks', _message)
+      }
+
+      execute({ codeBlocks, sendMessage })
     })
   })
 
